f9122271cf9871477190e9ace7b75ac8f0701224
Oluwadare Seyi <oluwadareseyii@gmail.com>
2021-05-25 22:51:29 +0100

    WIP: archive horizontal scroll

--- pages/archive.vue
+++ pages/archive.vue
@@ -14,21 +14,25 @@
     </div>
     <LocomotiveScroll
       ref="scroller"
+      class="horizontal"
       :getted-options="{
         direction: 'horizontal',
         smooth: true,
         getSpeed: true,
         lerp: 0.08,
         smartphone: {
-          smooth: true
+          smooth: true,
+          direction: 'vertical'
         },
         tablet: {
-          smooth: true
+          smooth: true,
+          direction: 'vertical',
+          breakpoint: 768
         }
         // Other options
       }"
     >
-      <div class="c-archive__main" ref="horizontalMain">
+      <div class="c-archive__main">
         <div>
           <div class="archive__intro">
             <p>
@@ -39,7 +43,7 @@
             <h1>
               “I try not to do everything, but I make sure I try everything.”
             </h1>
-            <nuxt-link to="/" class="home-link">
+            <nuxt-link to="/" class="home-link show-desktop">
               <animated-link class="c-bezier">
                 Go to Home
               </animated-link>
@@ -54,12 +58,6 @@
           </div>
           <div class="archive__images">
             <div class="c-image sm">
-              <!-- <img
-                data-scroll
-                data-scroll-speed="-0.8"
-                src="https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921592/Korty/Rectangle_34_faqgls.jpg"
-                alt=""
-              /> -->
               <div
                 data-scroll
                 data-scroll-speed="-0.8"
@@ -69,9 +67,148 @@
             </div>
 
             <div class="c-image xs">
-              <img
-                src="https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921598/Korty/Rectangle_25_wygie2.jpg"
-                alt=""
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921598/Korty/Rectangle_25_wygie2.jpg);"
+              />
+            </div>
+
+            <div class="c-image sm nobp">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921685/Korty/Rectangle_34-1_firxdx.jpg);"
+              />
+            </div>
+
+            <div class="c-image md nobp">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921602/Korty/Rectangle_36_lj8azl.jpg);"
+              />
+            </div>
+
+            <div class="c-image lg nobp">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921624/Korty/Rectangle_37_er4jkv.jpg);"
+              />
+            </div>
+
+            <div class="c-image md">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921596/Korty/Rectangle_38_qtfbi4.jpg);"
+              />
+            </div>
+
+            <div class="c-image xs">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921620/Korty/Rectangle_39_bewtbe.jpg);"
+              />
+            </div>
+
+            <div class="c-image sm">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921686/Korty/Rectangle_40_ear7xg.jpg);"
+              />
+            </div>
+
+            <div class="c-image sm">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921677/Korty/Rectangle_42_md72qx.jpg);"
+              />
+            </div>
+
+            <div class="c-image xs">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921618/Korty/Rectangle_43_xhdyyi.jpg);"
+              />
+            </div>
+
+            <div class="c-image xs">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921643/Korty/Rectangle_44_crnxqk.jpg);"
+              />
+            </div>
+
+            <div class="c-image md">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921622/Korty/Rectangle_41_czd9h1.jpg);"
+              />
+            </div>
+
+            <div class="c-image xs">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921687/Korty/Rectangle_45_omfqqx.jpg);"
+              />
+            </div>
+
+            <div class="c-image xs">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921750/Korty/Rectangle_46_hwl8fv.jpg);"
+              />
+            </div>
+
+            <div class="c-image sm">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921644/Korty/Rectangle_47_nesabh.jpg);"
+              />
+            </div>
+
+            <!-- To cpoy -->
+
+            <div class="c-image sm">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url(https://res.cloudinary.com/dmwfd0zhh/image/upload/q_auto,f_auto/v1621921644/Korty/Rectangle_47_nesabh.jpg);"
+              />
+            </div>
+
+            <div class="c-image">
+              <div
+                data-scroll
+                data-scroll-speed="-0.8"
+                class="bg-image"
+                style="background-image: url();"
               />
             </div>
           </div>
@@ -82,16 +219,44 @@
 </template>
 
 <script>
-import AnimatedArrow from "../components/AnimatedArrow.vue";
-import AnimatedLink from "../components/AnimatedLink/AnimatedLink.vue";
+import AnimatedArrow from "@/components/AnimatedArrow.vue";
+import AnimatedLink from "@/components/AnimatedLink/AnimatedLink.vue";
+import { map, clamp } from "@/utils/index";
 export default {
   components: { AnimatedArrow, AnimatedLink },
   mounted() {
-    const mainRef = this.$refs.horizontalMain;
-    const scrollWidth = mainRef.scrollWidth;
-    mainRef.style.width = scrollWidth;
-
+    const locomotive = this.$refs.scroller.locomotive;
     this.$nuxt.$emit("update-locomotive");
+
+    let direction = "horizontal";
+    window.addEventListener("resize", () => {
+      const width = window.innerWidth;
+
+      if (width <= 768 && direction !== "vertical") {
+        locomotive.destroy();
+        locomotive.init();
+        direction = "vertical";
+      }
+
+      if (width > 768 && direction === "vertical") {
+        locomotive.destroy();
+        locomotive.init();
+        direction = "horizontal";
+      }
+    });
+
+    let scroll = { cache: 0, current: 0 };
+    const allImgs = [...document.querySelectorAll(".archive__images .c-image")];
+    locomotive.on("scroll", obj => {
+      scroll.current = obj.scroll.x;
+      const distance = scroll.current - scroll.cache;
+      scroll.cache = scroll.current;
+      const skewVal = map(distance, -50, 50, -15, 15);
+      allImgs.forEach(
+        el => (el.style.transform = `skewX(${clamp(skewVal, -1, 1)}deg)`)
+      );
+    });
+    locomotive.update();
   }
 };
 </script>
